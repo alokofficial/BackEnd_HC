1609ca2f92a4912e17380f0fb084ccc1f5b0b91e
Alok Kumar <aloknh31@gmail.com>
2024-12-28 04:48:31 +0530

    connect to mongodb localhost or atlas

---
+++ Alok Backend/src/index.js
@@ -0,0 +1,73 @@
+// require('dotenv').config({ path: '../.env' });
+
+import dotenv from 'dotenv';
+import connectDB from './db/index.js';
+
+dotenv.config({ path: '.env' });
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* approch 0
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+            useCreateIndex: true,
+        });
+
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
+*/
+
+/*  approch 1
+
+import express from 'express';
+const app = express();
+;(async()=>{
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME} `)
+        app.on("error", (error) => {
+            console.error("error",error)
+            throw error
+        })
+        app.listen(process.env.PORT,()=>console.log(`server started on port ${process.env.PORT}`))
+    }catch(error){
+        console.error("error",error)
+    }
+})()
+    */
