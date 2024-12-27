d26c3474ee027a9d67f299dac9e43b63856c56ce
Alok Kumar <aloknh31@gmail.com>
2024-12-28 03:28:32 +0530

    backend

--- Learning Mongoose/models/hospital-management/medical_record.models.js
+++ Learning Mongoose/models/hospital-management/medical_record.models.js
@@ -1,5 +1,20 @@
 import mongoose from 'mongoose';
 
-const medicalRecordSchema = mongoose.Schema({},{timestamps:true})
+const medicalRecordSchema = mongoose.Schema({
+    patientId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Patient',
+        required:true
+    },
+    doctorId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Doctor',
+        required:true
+    },
+    medicalReport:{
+        type:String,
+        required:true
+    }
+},{timestamps:true})
 
 export const MedicalRecord = mongoose.model('MedicalRecord',medicalRecordSchema)
---
+++ readme.md
@@ -0,0 +1,3 @@
+![alt text](image-1.png)
+
+https://app.eraser.io/workspace/YtPqZ1VogxGy1jzIDkzj
